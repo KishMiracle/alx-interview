68e61d53f906902e2f71063b692c8c48de098815
KishMiracle <miraclekish@gmail.com>
2023-10-08 16:36:50 +0100

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-from collections import deque
-
-
 def canUnlockAll(boxes):
     if not boxes:
         return False
 
     n = len(boxes)
     visited = [False] * n
-    visited[0] = True
-    queue = deque([0])
 
-    while queue:
-        current_box = queue.popleft()
-        keys = boxes[current_box]
+    def dfs(box):
+        visited[box] = True
+        for key in boxes[box]:
+            if not visited[key]:
+                dfs(key)
 
-        for key in keys:
-            if 0 <= key < n and not visited[key]:
-                visited[key] = True
-                queue.append(key)
-
+    dfs(0)
     return all(visited)
