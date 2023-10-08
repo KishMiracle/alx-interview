ecc1a7d339675868ff2fad3fb6d474db1d95ab21
KishMiracle <miraclekish@gmail.com>
2023-10-09 00:39:29 +0100

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
+"""
+Task: Lockboxes
+"""
+
+
 def canUnlockAll(boxes):
-    if not boxes:
-        return False
-
-    n = len(boxes)
-    visited = [False] * n
-
-    def dfs(box):
-        visited[box] = True
-        for key in boxes[box]:
-            if not visited[key]:
-                dfs(key)
-
-    dfs(0)
-    return all(visited)
+    unlocked = [0]
+    for box_id, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != box_id:
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
