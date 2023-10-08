f0494082229f6b1dac0492da9966efe9692867bb
KishMiracle <miraclekish@gmail.com>
2023-10-09 00:45:54 +0100

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
+""" pascal triangle
+"""
+
+
 def pascal_triangle(n):
+    """ returns pascal triangle
+    """
     if n <= 0:
         return []
 
-    triangle = [[1]]
+    pas = []
 
-    for i in range(1, n):
-        row = [1]
-        prev_row = triangle[-1]
+    for i in range(n):
+        r_List = [1]
+        if i == 0:
+            pas.append(r_List)
+            continue
 
-        for j in range(1, i):
-            row.append(prev_row[j-1] + prev_row[j])
+        k = i - 1
+        for j in range(len(pas[k])):
+            if j + 1 == len(pas[k]):
+                r_List.append(1)
+                break
+            # adding the result of previous row and col
+            res = pas[k][j] + pas[k][j + 1]
+            r_List.append(res)
+        pas.append(r_List)
 
-        row.append(1)
-        triangle.append(row)
-
-    return triangle
+    return pas
